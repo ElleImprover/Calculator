2990eb1acbc11d1a32bac30ad6751c1db8130288
Elizabeth Davis <elizabeth.davis@improving.com>
2021-07-04 22:41:48 -0500

    Step 2 commit v2

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -19,26 +19,32 @@ namespace Calculator
                 Console.Out.WriteLine("Please enter the first number. Enter x to exit.");
                 string in1 = Console.ReadLine();
 
-                if (!in1.Equals("x", StringComparison.CurrentCultureIgnoreCase)) {
-                    Console.Out.WriteLine("Please enter the second number. Enter x to exit.");
-                    string in2 = Console.ReadLine();
+                if (!in1.Equals("x", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    success = Decimal.TryParse(in1, out input1);
+                    if (success)
+                    {
+                        Console.Out.WriteLine("Please enter the second number. Enter x to exit.");
+                        string in2 = Console.ReadLine();
 
-                    if (!in2.Equals("x", StringComparison.CurrentCultureIgnoreCase)) {
-                        success = Decimal.TryParse(in1, out input1);
-                        if (success) {
+                        if (!in2.Equals("x", StringComparison.CurrentCultureIgnoreCase)) {
+
                             success = Decimal.TryParse(in2, out input2);
-                            if (success) {
+                            if (success)  {
                                 Console.WriteLine("The sum is {0}.", input1 + input2);
                             }
+                            else {
+                                Console.WriteLine("Please enter a number.\nThe second entry is in an invalid format.");
+                            }
                         }
-                        if (!success) {
-                            Console.WriteLine("Please enter a number.\nThis is not a number.");
-                        } 
+                        else {
+                            done = true;
+                        }
                     }
                     else {
-                        done = true;
+                        Console.WriteLine("Please enter a number.\nThe first entry is in an invalid format.");
                     }
-                }
+          }
                 else {
                     done = true;
                 }
